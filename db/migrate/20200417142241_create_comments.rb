d456646616be6f13fda11051f1fb252191060a34
Alexandru <rammazzoti2000@gmail.com>
2020-04-17 21:53:48 +0200

    Add foreign key to Comment

--- db/migrate/20200417142241_create_comments.rb
+++ db/migrate/20200417142241_create_comments.rb
@@ -3,8 +3,6 @@ class CreateComments < ActiveRecord::Migration[6.0]
     create_table :comments do |t|
       t.string :title
       t.text :body
-      t.integer :user_id
-      t.integer :post_id
       t.references :user, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,12 @@ ActiveRecord::Schema.define(version: 2020_04_17_142241) do
   create_table "comments", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id"
-    t.integer "post_id"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -33,9 +35,12 @@ ActiveRecord::Schema.define(version: 2020_04_17_142241) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
